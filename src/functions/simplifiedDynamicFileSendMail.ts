f7f155a98d830cb81f807eccb1242a57222b52a1
Albert Conceição <developer.albert@outlook.com>
2024-11-04 11:04:34 -0300

    feat: receiving emailToSend through multpart form data

--- src/functions/simplifiedDynamicFileSendMail.ts
+++ src/functions/simplifiedDynamicFileSendMail.ts
@@ -3,7 +3,7 @@ import { nodemailerClient } from '../clients/nodemailerClient';
 import { response } from '../utils/response';
 
 export async function handler(event:any) {
-  const { files } = await parser.parse(event);
+  const { files, emailToSend } = await parser.parse(event);
     const [file] = files;
 
     console.log(file);
@@ -14,23 +14,22 @@ export async function handler(event:any) {
         });
     }
 
-    if (file.contentType !== 'image/png') {
-        return response(400, {
-            error: 'Only png files are accepted',
-        });
-    }
+    const fileContent = file.contentType.startsWith('image/') || file.contentType === 'application/pdf'
+    ? file.content
+    : file.content;
 
   await nodemailerClient.sendMail({
     from: 'Sandy Carvalho <contato@sandycarvalho.com.br>',
     replyTo: ['contato@sandycarvalho.com.br', 'agendamento@sandycarvalho.com.br'],
-    to: ['contato@albertconceicao.dev.br', 'developer.albert@outlook.com'],
+    to: [emailToSend],
     subject: 'Você foi cadastrado na clínica virtual da Psicóloga Sandy Carvalho',
     text: 'Estamos felizes de ter você conosco, que sua jornada de autoconhecimento seja maravilhosa',
     html: '<h1>Olá, tudo bem?</h1><p>Estamos felizes de ter você conosco, que sua <strong>jornada de autoconhecimento</strong> seja maravilhosa</p>',
     attachments: [
       {
         filename: file.filename,
-        content: file.content,
+        content: fileContent,
+        contentType: 'application/pdf',
       }
     ]
   });
